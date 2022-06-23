a11f29b6f6b1ec7cf82821fcac9d6ac9f4a78594
Rogelio Valle <evenicervalle@gmail.com>
2022-06-22 20:21:16 -0500

    Data manipulation and transformation with Numpy

---
+++ Data Manipulation and Transformation/Arrays.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+lista = [1,2,3,4,5,6,7,8,9]
+
+arr = np.array(lista)
+type(arr)
+
+matriz = [[1,2,3], [4,5,6], [7,8,9]]
+matriz = np.array(matriz)
+#print(matriz)
+
+#print(arr[0])
+
+#print(matriz[0,0])
+
+print(arr[0:3])
